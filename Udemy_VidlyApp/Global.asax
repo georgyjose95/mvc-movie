3615ec0d063ed66f140c4eb56c3c084450cf208b
ASUS\ASUS-PC <georgyjose95@gmail.com>
2018-02-07 12:38:53 +0530

    Add project files.

---
+++ Udemy_VidlyApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Udemy_VidlyApp.MvcApplication" Language="C#" %>
---
+++ Udemy_VidlyApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Udemy_VidlyApp.Startup))]
+namespace Udemy_VidlyApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
