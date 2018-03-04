d9bcfb14d23e92df827606dafee224320659c185
ASUS\ASUS-PC <georgyjose95@gmail.com>
2018-03-05 00:50:20 +0530

    Customer Model

---
+++ Udemy_VidlyApp/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Udemy_VidlyApp.Models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Dont Forget to enter your name!")][StringLength(255)]
+        public string Name { get; set; }
+        public  bool IsSuscribed{ get; set; }
+        public MemberShipType MemberShipType { get; set; }
+        [Display(Name =" Membership Type")]
+        public byte MemberShipTypeId { get; set; } 
+      
+        [Display(Name= "Date of Birth")]
+        [ValidateMemberByAge]
+        public DateTime? Dob { get; set; }
+
+
+
+
+    }
+}
