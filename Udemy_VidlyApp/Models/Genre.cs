2d2eb9bd60c421f0926a50feb1487c9b9530e173
ASUS\ASUS-PC <georgyjose95@gmail.com>
2018-03-05 00:52:12 +0530

    Model commits

---
+++ Udemy_VidlyApp/Models/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Udemy_VidlyApp.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Udemy_VidlyApp/Models/ValidateMemberByAge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Udemy_VidlyApp.Models
+{
+    public class ValidateMemberByAge: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = (Customer)validationContext.ObjectInstance;
+            
+            if(customer.MemberShipTypeId == MemberShipType.Unknown || customer.MemberShipTypeId == MemberShipType.PayAsYouGo)
+            {
+                return ValidationResult.Success;
+            }
+            if (customer.Dob == null)
+            {
+                return new ValidationResult("Birthdate must be specified!");
+            }
+            var age = DateTime.Today.Year - customer.Dob.Value.Year;
+
+            return (age>=18)? ValidationResult.Success: new ValidationResult("Customer age must be greater than 18 ");
+        }
+
+    }
+}
