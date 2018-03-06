4b4d3fae8607d1f7ae13fac00c5316596456162d
ASUS\ASUS-PC <georgyjose95@gmail.com>
2018-03-07 00:38:57 +0530

    Web APIs

--- Udemy_VidlyApp/Global.asax.cs
+++ Udemy_VidlyApp/Global.asax.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Udemy_VidlyApp.App_Start;
 
 namespace Udemy_VidlyApp
 {
@@ -12,6 +15,8 @@ namespace Udemy_VidlyApp
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
