afad6f2fdccc96ccb2dc8c88e572f49e9fd0d899
ASUS\ASUS-PC <georgyjose95@gmail.com>
2018-03-07 16:52:59 +0530

    Web Apis movie commit II

---
+++ Udemy_VidlyApp/Controllers/api/MoviesController.cs
@@ -0,0 +1,79 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Udemy_VidlyApp.Dtos;
+using Udemy_VidlyApp.Models;
+
+namespace Udemy_VidlyApp.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<MoviesDto> GetMovies(Movie movies)
+        {
+            return _context.Movie.ToList().Select(Mapper.Map<Movie, MoviesDto>);
+        }
+
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movie.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+            return Ok(Mapper.Map<Movie, MoviesDto>(movie));
+        }
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MoviesDto moviesDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var movie = Mapper.Map<MoviesDto, Movie>(moviesDto);
+
+            _context.Movie.Add(movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), moviesDto);
+        }
+
+    
+       [HttpPut]
+        public void UpdateMovie(int id, MoviesDto moviesDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var movieInDb = _context.Movie.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            Mapper.Map<MoviesDto, Movie>(moviesDto, movieInDb);
+
+            _context.SaveChanges(); 
+
+        }
+        [HttpDelete]
+        public void DeleteMovie(int id,MoviesDto moviesDto)
+        {
+            var movieInDb = _context.Movie.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movie.Remove(movieInDb);
+        }
+        
+    }
+}
---
+++ Udemy_VidlyApp/Dtos/MoviesDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Udemy_VidlyApp.Dtos
+{
+    public class MoviesDto
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Movie name required")]
+
+        public string Name { get; set; }
+       
+
+        [Required(ErrorMessage = "Please specify the Release date!")]
+        public DateTime ReleaseDate { get; set; }
+        
+        [Required(ErrorMessage = "Please specify the Date added!")]
+        public DateTime DateAdded { get; set; }
+        [Range(0, 20)]
+        [Required(ErrorMessage = "The stock must be between 0 - 20")]
+        public int Stock { get; set; }
+
+        [Required(ErrorMessage = "Please select the Genre Type")]
+        public int GenreId { get; set; }
+    }
+}
